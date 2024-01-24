b271d109c3524a63b151ee305f8d6cb18818f309
Thomas <145229175+t-zl@users.noreply.github.com>
2024-01-24 16:07:09 +0000

    Add files via upload

---
+++ Java Code/Hello_World.java
@@ -0,0 +1,5 @@
+public class Hello_World{ // Everything in java is a class.
+    public static void main(String[] args) {    //All programs must have main().
+        System.out.println("Hello World!");
+    }
+}
